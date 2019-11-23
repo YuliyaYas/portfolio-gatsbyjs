29bca614ff110efd76fb0f52c07f853333e3d536
Yuliya <jasenetska@gmail.com>
2019-11-23 18:58:49 -0500

    dynamically adding pages from cms

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,21 +6,27 @@ const data = graphql`
     file(relativePath: {eq: "logo_yy.png"}) {
       relativePath
     }
+    allContentfulArtPage {
+      nodes {
+        title
+      }
+    }
   }  
 `
 const Navbar = () => {
     const logo = useStaticQuery(data).file.relativePath
+    const pages = useStaticQuery(data).allContentfulArtPage.nodes
+    const menu = pages.map(({title}, i) => {
+      return (<li key={i}><Link to={title}>{title.toUpperCase()}</Link></li>)
+    })
     return (
         <nav>
             <a href="/"><img id="logo-mobile" src={require(`../images/${logo}`)} alt="yasenetska logo"/></a>
             <ul>
-                <li><Link to='/paintings'>Paintings</Link></li>
-                <li><Link to='/illustrations'>Illustrations</Link></li>
+              {menu}
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/about'>About</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
-                
-
+                <li><Link to='/contact'>Contact</Link></li>   
             </ul>
         </nav>
     )
